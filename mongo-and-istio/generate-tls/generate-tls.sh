ace8d14fa898d24cde80b00715263ebe98dd949f
spron-in <sergey.pronin@percona.com>
2024-04-11 19:02:48 +0300

    mongo-istio

---
+++ mongo-and-istio/generate-tls/generate-tls.sh
@@ -0,0 +1,85 @@
+CLUSTER_NAME=rs-psmdb-db
+NAMESPACE=default
+SECRET_NAME=rs-psmdb-db-tls
+MYDOMAIN=.percona.spron.in
+
+cat <<EOF | cfssl gencert -initca - | cfssljson -bare ca
+  {
+    "CN": "Root CA",
+    "names": [
+      {
+        "O": "PSMDB"
+      }
+    ],
+    "key": {
+      "algo": "rsa",
+      "size": 2048
+    }
+  }
+EOF
+
+cat <<EOF > ca-config.json
+  {
+    "signing": {
+      "default": {
+        "expiry": "87600h",
+        "usages": ["signing", "key encipherment", "server auth", "client auth"]
+      }
+    }
+  }
+EOF
+
+cat <<EOF | cfssl gencert -ca=ca.pem  -ca-key=ca-key.pem -config=./ca-config.json - | cfssljson -bare server
+  {
+    "hosts": [
+      "localhost",
+      "${CLUSTER_NAME}-rs0",
+      "${CLUSTER_NAME}-rs0.${NAMESPACE}",
+      "${CLUSTER_NAME}-rs0.${NAMESPACE}.svc.cluster.local",
+      "*.${CLUSTER_NAME}-rs0",
+      "*.${CLUSTER_NAME}-rs0.${NAMESPACE}",
+      "*.${CLUSTER_NAME}-rs0.${NAMESPACE}.svc.cluster.local",
+      "*.${MYDOMAIN}"
+    ],
+    "names": [
+      {
+        "O": "PSMDB"
+      }
+    ],
+    "CN": "${CLUSTER_NAME/-rs0}",
+    "key": {
+      "algo": "rsa",
+      "size": 2048
+    }
+  }
+EOF
+
+cfssl bundle -ca-bundle=ca.pem -cert=server.pem | cfssljson -bare server
+
+kubectl create secret generic ${SECRET_NAME}-internal --from-file=tls.crt=server.pem --from-file=tls.key=server-key.pem --from-file=ca.crt=ca.pem --type=kubernetes.io/tls
+
+cat <<EOF | cfssl gencert -ca=ca.pem  -ca-key=ca-key.pem -config=./ca-config.json - | cfssljson -bare client
+  {
+    "hosts": [
+      "${CLUSTER_NAME}-rs0",
+      "${CLUSTER_NAME}-rs0.${NAMESPACE}",
+      "${CLUSTER_NAME}-rs0.${NAMESPACE}.svc.cluster.local",
+      "*.${CLUSTER_NAME}-rs0",
+      "*.${CLUSTER_NAME}-rs0.${NAMESPACE}",
+      "*.${CLUSTER_NAME}-rs0.${NAMESPACE}.svc.cluster.local",
+      "*.${MYDOMAIN}"
+    ],
+    "names": [
+      {
+        "O": "PSMDB"
+      }
+    ],
+    "CN": "${CLUSTER_NAME/-rs0}",
+    "key": {
+      "algo": "rsa",
+      "size": 2048
+    }
+  }
+EOF
+
+kubectl create secret generic ${SECRET_NAME} --from-file=tls.crt=client.pem --from-file=tls.key=client-key.pem --from-file=ca.crt=ca.pem --type=kubernetes.io/tls
